6ad1b18968b128534c373116d4ac7d332bf71d9d
Laurentiu Cerghedean <laurentiu.cerghedean@yahoo.com>
2023-01-19 16:06:21 +0200

    Creare meniu - front end basics

--- index.php
+++ index.php
@@ -1,20 +1,49 @@
 <!DOCTYPE html>
 <html lang="">
 <head>
-				<meta charset="utf-8">
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<title>E-Store - Magazin online de suplimente alimentare</title>
-				<link rel="stylesheet" href="styles/bootstrap-337.min.css">
-				<link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
-				<link rel="stylesheet" href="styles/style.css">
-
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>E-Store - Magazin online de suplimente alimentare</title>
+	<link rel="stylesheet" href="styles/bootstrap-337.min.css">
+	<link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="styles/style.css">
 </head>
 
 <body>
 
+	<div id="top"><!--Top begin-->
+		<div class="container"><!--container begin-->
+			<div class="col-md-6 offer"><!--col-md-6 offer begin-->
 
+				<a href="#" class="btn btn-success btn-sm">Welcome</a>
+				<a href="checkout.php">4 Produse in Cos | Pret total: 300 RON</a>
 
-				<script src="js/jquery-331.min.js"></script>
-				<script src="js/bootstrap-337.min.js"></script>
+			</div><!--col-md-6 offer end-->
+
+			<div class="col-md-6"><!--col-md-6 begin-->
+
+				<ul class="menu"><!--menu begin-->
+
+					<li>
+						<a href="cutomer_register.php">Inregistrare</a>
+					</li>
+					<li>
+						<a href="checkout.php">Contul meu</a>
+					</li>
+					<li>
+						<a href="cart.php">Cos cumparaturi</a>
+					</li>
+					<li>
+						<a href="checkout.php">Login</a>
+					</li>
+
+				</ul><!--menu end-->
+
+			</div><!--col-md-6 end-->
+		</div><!--container end-->
+	</div><!--Top end-->
+
+	<script src="js/jquery-331.min.js"></script>
+	<script src="js/bootstrap-337.min.js"></script>
 </body>
 </html>
