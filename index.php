24dc5dbd402723d969d0eb4e5a9984c4ba1549b0
Laurentiu Cerghedean <laurentiu.cerghedean@yahoo.com>
2023-01-19 15:15:14 +0200

    Link-uire resurse front end

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>E-Store - Magazin online de suplimente alimentare</title>
+				<link rel="stylesheet" href="styles/bootstrap-337.min.css">
+				<link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+				<link rel="stylesheet" href="styles/style.css">
+
+</head>
+
+<body>
+
+
+
+				<script src="js/jquery-331.min.js"></script>
+				<script src="js/bootstrap-337.min.js"></script>
+</body>
+</html>
